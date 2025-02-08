a6f511336695c3446850265961c5bf605da097af
SWENoob <124020801+SWENoob@users.noreply.github.com>
2025-02-08 23:27:58 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
-const noButton = document.getElementById("id-no");
-const yesButton = document.getElementById("id-yes");
-fixYesButton();
-function fixYesButton () {
-    const yesData = yesButton.getBoundingClientRect();
-    const y = yesData.y
-    const height = yesData.height;
-    noButton.style.top = `${y + height}px`;
-    console.log(y + height);
-    console.log(noButton.getBoundingClientRect())
-    
-}
+document.addEventListener("DOMContentLoaded", (event) => {
+    const noButton = document.getElementById("id-no");
+    const yesButton = document.getElementById("id-yes");
+    fixYesButton();
+    function fixYesButton () {
+        const yesData = yesButton.getBoundingClientRect();
+        const y = yesData.y
+        const height = yesData.height;
+        noButton.style.top = `${y + height}px`;
+        console.log(y + height);
+        console.log(noButton.getBoundingClientRect())
+        
+    }
 
-function randomNumber (min, max) {
-    return Math.floor(Math.random() * (max-min+1)) + min;
-}
+    function randomNumber (min, max) {
+        return Math.floor(Math.random() * (max-min+1)) + min;
+    }
 
-function pictureChange() {
-    document.getElementsByClassName('gif').src="snoopy-cry.gif";
-}
+    function pictureChange() {
+        document.getElementsByClassName('gif').src="snoopy-cry.gif";
+    }
 
-noButton.addEventListener('click', () => {
-    const bodyElement = document.getElementById("bo");
-    const bodyData = bodyElement.getBoundingClientRect();
+    noButton.addEventListener('click', (e) => {
+        const bodyElement = document.getElementById("bo");
+        const bodyData = bodyElement.getBoundingClientRect();
 
-    let x = randomNumber(10, bodyData.width - 130);
-    let y = randomNumber(10, bodyData.height - 80);
-    BigInt.innterText = "Press Yes!"
-    event.target.style.left = `${x}px`;
-    event.target.style.top = `${y}px`;
+        let x = randomNumber(10, bodyData.width - 130);
+        let y = randomNumber(10, bodyData.height - 80);
+        BigInt.innterText = "Press Yes!"
+        e.target.style.left = `${x}px`;
+        e.target.style.top = `${y}px`;
+    });
 });
+  
