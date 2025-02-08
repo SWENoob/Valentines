6fa525c1c9e224286f55619bd8d6b3205466e808
SWENoob <124020801+SWENoob@users.noreply.github.com>
2025-02-08 20:51:32 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-const noButton = document.getElementById("id_no");
+const noButton = document.getElementById("id-no");
+function randomNumber (min, max) {
+    return Math.floor(Math.random() * (max-min+1)) + min;
+}
 noButton.addEventListener('click', () => {
-    alert('Nice try')
-})
+    let num1 = randomNumber(20, 300);
+    let num2 = randomNumber(30, 600);
+    BigInt.innterText = "Press Yes!"
+    console.log(num1 + ' ' + num2);
+    event.target.style.left = `${num1}px`;
+    event.target.style.top = `${num2}px`;
+});
