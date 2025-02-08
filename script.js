a2f099a484428f9dfeb14e0b16ad6dd26252ed2b
SWENoob <124020801+SWENoob@users.noreply.github.com>
2025-02-08 21:14:37 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const noButton = document.getElementById("id-no");
 function randomNumber (min, max) {
     return Math.floor(Math.random() * (max-min+1)) + min;
 }
+
+function pictureChange() {
+    document.getElementsByClassName('gif').src="snoopy-cry.gif";
+}
+
 noButton.addEventListener('click', () => {
     let num1 = randomNumber(20, 300);
     let num2 = randomNumber(30, 600);
